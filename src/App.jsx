86c9b1ab43c5d4f98af018d7597bd1cf759091a4
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-06-28 21:29:37 +0530

    Made all api call parallel

--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,27 @@ const App = () => {
   const [badge, setBadge] = useState({ badgesCount: 0, badges: [] });
 
   const handleApi = async () => {
-    if (search.trim() === "") return;
-    const fetchProfile = await fetch(
-      `https://alfa-leetcode-api.onrender.com/${search}`,
-    );
-    const profileData = await fetchProfile.json();
-    setProfile(profileData);
-    const fetchStats = await fetch(
-      `https://alfa-leetcode-api.onrender.com/${search}/profile`,
-    );
-    const statsData = await fetchStats.json();
-    setStats(statsData);
-    const fetchBadges = await fetch(
-      `https://alfa-leetcode-api.onrender.com/${search}/badges`,
-    );
-    const badgesData = await fetchBadges.json();
-    setBadge(badgesData);
+    if (!search.trim()) return;
+
+    try {
+      const [profileRes, statsRes, badgesRes] = await Promise.all([
+        fetch(`https://alfa-leetcode-api.onrender.com/${search}`),
+        fetch(`https://alfa-leetcode-api.onrender.com/${search}/profile`),
+        fetch(`https://alfa-leetcode-api.onrender.com/${search}/badges`),
+      ]);
+
+      const [profileData, statsData, badgesData] = await Promise.all([
+        profileRes.json(),
+        statsRes.json(),
+        badgesRes.json(),
+      ]);
+
+      setProfile(profileData);
+      setStats(statsData);
+      setBadge(badgesData);
+    } catch (error) {
+      console.error(error);
+    }
   };
   return (
     <div>
